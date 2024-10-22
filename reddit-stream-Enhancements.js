5b549137fbc3c7b01c7cbe36332acca78f2a5c6f
Nick2bad4u <20943337+Nick2bad4u@users.noreply.github.com>
2024-10-21 23:59:45 -0400

    Update reddit-stream-Enhancements.js

--- reddit-stream-Enhancements.js
+++ reddit-stream-Enhancements.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Scroll to Bottom, Stay Button, and Auto-Reload with Persistence and Icons for reddit-stream.com
 // @namespace    http://tampermonkey.net/
-// @version      2.2
+// @version      2.3
 // @description  Adds a floating button to scroll to the bottom, checkboxes to stay at the bottom, and to reload the page every 10 or 30 seconds on reddit-stream.com, with persistent settings and icons for clarity.
 // @author       Nick2bad4u
 // @match        *://*.reddit-stream.com/*
