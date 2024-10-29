888a7645d384558da1cfc495d890bbf5be868610
Nick2bad4u <20943337+Nick2bad4u@users.noreply.github.com>
2024-10-29 10:20:11 -0400

    Create StravaAutoAuthorize.user.js

---
+++ StravaAutoAuthorize.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Strava-Auto-Authorize
+// @namespace    nick2bad4u
+// @description  Auto allows Strava apps without having to click "authorize" every single time.
+// @author       Nick2bad4u
+// @match        https://www.strava.com/oauth/authorize*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=strava.com
+// @version      2.0
+// @grant        none
+// @homepage     https://github.com/Nick2bad4u/StravaAutoAuthorize
+// @homepageURL  https://github.com/Nick2bad4u/StravaAutoAuthorize
+// @supportURL   https://github.com/Nick2bad4u/StravaAutoAuthorize/issues
+// @downloadURL  https://raw.githubusercontent.com/Nick2bad4u/StravaAutoAuthorize/main/StravaAutoAuthorize.js
+// @license CC-BY-SA-3.0; http://creativecommons.org/licenses/by-sa/3.0/
+// @license MIT
+// ==/UserScript==
+
+setTimeout(function() {
+    document.getElementsByClassName("btn")[0].click();
+}, 1000)
