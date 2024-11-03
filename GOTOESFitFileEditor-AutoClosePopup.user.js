60f27c9563ad8f3ace4521221be0872a3d2e6a8f
Nick2bad4u <20943337+Nick2bad4u@users.noreply.github.com>
2024-11-03 18:25:14 -0500

    Update GOTOESFitFileEditor-AutoClosePopup.user.js

--- GOTOESFitFileEditor-AutoClosePopup.user.js
+++ GOTOESFitFileEditor-AutoClosePopup.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GOTOES Fit File Editor - Auto Close Popup
 // @namespace    https://userstyles.github.typpi.online
-// @version      2024.11.04
+// @version      2024.11.05
 // @description  Automatically clicks the close button on popup donation nag
 // @author       Nick2bad4u
 // @match        https://gotoes.org/*
