759b428717e70d10664855c23580b13971273ff5
Nick2bad4u <20943337+Nick2bad4u@users.noreply.github.com>
2024-11-10 11:22:42 -0500

    Update OldRedditNewProfilePictures-API-Key-Version-Reddit-Stream-Version.user.js

--- OldRedditNewProfilePictures-API-Key-Version-Reddit-Stream-Version.user.js
+++ OldRedditNewProfilePictures-API-Key-Version-Reddit-Stream-Version.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Old Reddit with New Reddit Profile Pictures - API Key Version - Reddit-Stream Version
 // @namespace    https://github.com/Nick2bad4u/UserStyles
-// @version      7.0.1
+// @version      7.0.2
 // @description  Injects new Reddit profile pictures into Old Reddit and Reddit-Stream.com next to the username. Caches in localstorage. This version requires an API key. Enter your API Key under CLIENT_ID and CLIENT_SECRET or it will not work.
 // @author       Nick2bad4u
 // @match        *://reddit-stream.com/*
@@ -26,7 +26,7 @@
   // Reddit API credentials
   const CLIENT_ID = 'EnterClientIDHere';
   const CLIENT_SECRET = 'EnterClientSecretHere';
-  const USER_AGENT = 'ProfilePictureInjector/7.0.1 by Nick2bad4u';
+  const USER_AGENT = 'ProfilePictureInjector/7.0.2 by Nick2bad4u';
   let accessToken = localStorage.getItem('accessToken');
 
   // Retrieve cached profile pictures and timestamps from localStorage
