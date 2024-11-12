9cecef85797351c00da8ea3cb3c72c2450946643
Nick2bad4u <20943337+Nick2bad4u@users.noreply.github.com>
2024-11-12 10:36:14 -0500

    Update YoutubeVolumeDisplay.user.js

--- YoutubeVolumeDisplay.user.js
+++ YoutubeVolumeDisplay.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube Volume Display
 // @namespace    https://github.com/Nick2bad4u/UserStyles
-// @version      1.1
+// @version      1.2
 // @description  Display current YouTube volume level in the UI
 // @match        *://www.youtube.com/*
 // @grant        none
