b0c659d53ae0c5d3584ff8a9ce9decef0236b1ae
Nick2bad4u <20943337+Nick2bad4u@users.noreply.github.com>
2024-12-11 23:17:25 -0500

    up

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,52 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
-import tseslint from 'typescript-eslint';
-
-export default [
-	{ files: ['**/*.{js,mjs,cjs,ts}'] },
-	{
-		files: ['**/*.js'],
-		languageOptions: { sourceType: 'script' },
-	},
-	{
-		languageOptions: { globals: globals.browser },
-	},
-	pluginJs.configs.recommended,
-	...tseslint.configs.recommended,
-];
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true, // If you are using Node.js
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:json/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'html',
+    'json',
+  ],
+  rules: {
+    'semi': ['error', 'always'],
+    'quotes': ['error', 'single'],
+    'no-unused-vars': 'warn',
+    'react/prop-types': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  overrides: [
+    {
+      files: ['**/*.css'],
+      processor: 'stylelint-processor-styled-components',
+    },
+    {
+      files: ['**/*.html'],
+      processor: 'eslint-plugin-html',
+    },
+    {
+      files: ['**/*.json'],
+      processor: 'eslint-plugin-json',
+    },
+  ],
+};
