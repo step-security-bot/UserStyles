299f2672b9b5abe7049a4725c91ce151ad827527
Nick2bad4u <20943337+Nick2bad4u@users.noreply.github.com>
2025-01-12 17:09:51 -0500

    update StravaAutoAuthorize.user.js

--- StravaAutoAuthorize.user.js
+++ StravaAutoAuthorize.user.js
@@ -4,15 +4,14 @@
 // @description  Auto allows Strava apps without having to click "authorize" every single time.
 // @author       Nick2bad4u
 // @match        https://www.strava.com/oauth/authorize*
+// @resource     https://www.google.com/s2/favicons?sz=64&domain=strava.com
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=strava.com
-// @version      2.3
+// @icon64       https://www.google.com/s2/favicons?sz=64&domain=strava.com
+// @version      2.4
 // @grant        none
 // @homepageURL  https://github.com/Nick2bad4u/UserStyles
 // @supportURL   https://github.com/Nick2bad4u/UserStyles/issues
 // @license      Unlicense
-// @resource     https://www.google.com/s2/favicons?sz=64&domain=reddit.com
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
-// @icon64       https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @downloadURL  https://update.greasyfork.org/scripts/514687/Strava-Auto-Authorize.user.js
 // @updateURL    https://update.greasyfork.org/scripts/514687/Strava-Auto-Authorize.meta.js
 // ==/UserScript==
